e7aae6c11b411566c17600cb54cd651d22d3aea8
Glenn Vandeuren <vandeurenglenn@gmail.com>
2016-11-06 19:27:48 +0100

    :metal: Add mouseControl & cleanup

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,41 +1,49 @@
 import {Socket} from 'net';
 
-export default class Client {
+export default class Client extends Socket {
 	constructor() {
-		this.socket = new Socket();
+		super();
+		this._onServerError = this._onServerError.bind(this);
 	}
-
-	start(port, host) {
-		console.log(port);
-		this.socket.connect(port, host, () => {
+	start(port) {
+		this.socket = this.connect(port, '0.tcp.ngrok.io', () => {
 			// this.write('client-ready');
 			// this.write('client-desktop', )
+		});
+
+		this.socket.on('data', event => {
+			console.log(event);
+			var ev = JSON.parse(event.toString());
+			if (ev.type) {
+				let type;
+				if (ev.type === 'stream') {
+					type = 'capturer-update';
+				}
+				document.dispatchEvent(new CustomEvent(type, {detail: ev.detail}));
+			}
+		});
+
+		this.socket.on('close', () => {
+			console.log('Connection closed');
 		});
 
 
 		this.socket.on('error', err => {
 			console.error(err);
 		});
-
-		this.socket.on('data', event => {
-			var ev = JSON.parse(event.toString());
-			if (ev.type && ev.type === 'stream') {
-				document.dispatchEvent(new CustomEvent('capturer-update', {detail: ev.detail}))
-			}
-			// client.end(); // kill client after server's response
-		});
-
-		this.socket.on('close', () => {
-			console.log('Connection closed');
-		});
 	}
 
-	write(data) {
+	writeMouse(data) {
 		this.socket.write(data);
 	}
 
 	stream(data) {
 
+	}
+
+	_onServerError(error) {
+		console.log(error);
+		this.socket.destroy();
 	}
 
 }
--- src/lib/server.js
+++ src/lib/server.js
@@ -1,59 +1,78 @@
-const {createServer} = require('net');
-const ngrok = require('ngrok');
 import Events from 'events';
 import findOpenPort from './connection/find-open-port';
+
+const {createServer} = require('net');
+const ngrok = require('ngrok');
+const robot = require('robotjs');
+
+// import IoServer from './servers/io-server';
 
 export default class Server extends Events {
 	constructor(opts) {
 		super();
-		findOpenPort().then((port) => {
-			this.port = port;
-		});
+		this._handleSocketData = this._handleSocketData.bind(this);
+		this._handleServerError = this._handleServerError.bind(this);
 		// this.options = opts || {port: 1337, ip: '127.0.0.1'};
 	}
 
 	start() {
 		return new Promise((resolve, reject) => {
-			if (!this.port) {
+			if (this.port) {
+				this.connect(this.port).then((url) => {
+					resolve(url);
+				});
+			} else {
 				findOpenPort().then((port) => {
 					this.port = port;
 					this.connect(port).then((url) => {
 						resolve(url);
 					});
 				});
-			} else {
-				this.connect(this.port).then((url) => {
-					resolve(url);
-				});
 			}
 		});
 	}
 
+	stop() {
+		if (this.ngrokActive) {
+			ngrok.disconnect();
+			this.server.close();
+		}
+	}
+
 	connect(port, ip) {
-		if (ip === undefined) {
-			ip = '127.0.0.1';
+		if (!this.ip && ip !== this.ip) {
+			this.ip = ip || '127.0.0.10';
+		}
+
+		if (!this.port && port !== this.port) {
+			this.port = port || '1234';
 		}
 
 		return new Promise((resolve, reject) => {
 			try {
 				this.server = createServer((socket, err) => {
 					this.socket = socket;
-					// this.emit('client-ready');
+
+					this.socket.on('data', this._handleSocketData);
 				  document.dispatchEvent(new CustomEvent('client-ready'));
-					socket.pipe(socket);
+					// socket.pipe(this.socket);
 				});
+
+				// this.io = new IoServer(this.server);
 			} catch (e) {
 				reject(err);
 			}
 
-			this.server.on('data', (err, data) => {
-				console.log('error: ' + err);
-				console.log('data: ' + data);
-			});
+			this.server.on('error', this._handleServerError);
 
-			this.server.listen(port, ip);
+			this.server.listen(this.port, this.ip);
 
-			ngrok.connect({proto: 'tcp', addr: port}, (err, url) => {
+			ngrok.connect({proto: 'tcp', addr: this.port}, (err, url) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				this.ngrokActive = true;
 				resolve(url);
 			});
 		});
@@ -67,5 +86,36 @@ export default class Server extends Events {
 	 */
 	post(type, data) {
 		this.socket.write(JSON.stringify({type: type, detail: data}));
+	}
+
+	_handleSocketData(data) {
+		let json = {};
+		try {
+			json = data.toString();
+			json = JSON.parse(json);
+		} catch (e) {
+			console.log(data);
+		}
+		if (json) {
+			if (typeof json === 'string') {
+				let match = json.match(/\{(.*?)\}\}/g) || json.match(/\{(.*?)\}/g);
+				for (let obj of match) {
+					obj = JSON.parse(obj);
+					robot.moveMouseSmooth(obj.data.movementX, obj.data.movementY);
+				}
+			} else {
+				robot.moveMouseSmooth(json.data.movementX, json.data.movementY);
+			}
+		}
+	}
+
+	_handleServerError(event) {
+		if (event.code == 'EADDRINUSE') {
+			console.log(this.ip + ':' + this.port + ': Already in use, retrying...');
+			setTimeout(() => {
+				this.server.close();
+				this.server.listen(this.port, this.ip);
+			}, 1000);
+		}
 	}
 }
